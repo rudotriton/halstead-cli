4ca0f636289bb72ffc4611402d904cad19efacbf
Raigo Jerva <raigo.jerva@gmail.com>
2019-11-26 20:34:19 +0000

    remove commented code

--- utils/getFiles.js
+++ utils/getFiles.js
@@ -7,22 +7,3 @@ module.exports = async (sourceDir) => {
   const files = await readdirAsync(sourceDir);
   return files;
 }
-
-// fs.readdir(sourceDir, (_, files) => {
-//   const numFiles = files.length;
-//   let source = '';
-
-//   files.forEach((file) => {
-//     source += fs.readFileSync(
-//       path.resolve(process.env.PWD, sourceDir, file),
-//       'utf8'
-//     );
-//     source += '\n';
-//   });
-//   const lines = source.match(/\n/g).length;
-//   const whiteSpaceLines = source.match(/(^[ \t]*\n)/gm).length;
-
-//   const parsedCode = recast.parse(source);
-//   const metrics = halstead(parsedCode.tokens);
-//   printMetrics({ ...metrics, lines, numFiles, whiteSpaceLines });
-// });
